f33cd5db5bc5960dc2f3fb5b4e8b8c5fab93b6b7
Joni Mustaniemi <mustaniemijm@gmail.com>
2019-11-07 17:18:14 +0200

    card id fix

--- js/index.js
+++ js/index.js
@@ -29,15 +29,24 @@
  var playerLevelTitle;
  var rings;
  var playerGearTitle;
+ var playerStrengthTitle;
+ var playerStrength;
+ var playerStrengthContainer;
  var playerLevelContainer;
  var playerGearContainer;
  var createPlayerCard;
  var playerGear;
+ var addGearIcon;
  var plusIcon;
+ var munchkinName;
  var minusIcon;
- var name = "name";
+ var maleIconIsActive = false;
+ var femaleIconIsActive = false;
+ var name = "";
  var level = 1;
  var gear = 0;
+ var id = 0;
+ var strength = level+gear;
  var munchkins = [];
  var maleIcon;
  var femaleIcon;
@@ -47,20 +56,23 @@
  document.getElementById("test").addEventListener("click", display);
  document.getElementById("maleIcon").addEventListener("click", setActive);
  document.getElementById("femaleIcon").addEventListener("click", setActive);
+ 
+
 
 function createPlayer() {
-  
-   
   blur = document.getElementById("blur");
   addButton = document.getElementById("addPlayer");
   createPlayerCard = document.getElementById("createPlayerCard");
-
-
+ 
   blur.style = "filter: blur(3px);";
   createPlayerCard.style = "display:block;";
   rings = document.getElementById('outerCircle');
   rings.style = "animation: rotation 9s infinite linear;";
   
+}
+
+function addMunchkinLevel() {
+this.level+=1;
 }
 
 function setActive() {
@@ -69,11 +81,15 @@ function setActive() {
 
     if(this.id == "maleIcon") {
         maleIcon.style = "filter: invert(100%);";
-        femaleIcon.style = "ilter: invert(0%);"
+        femaleIcon.style = "ilter: invert(0%);";
+        maleIconIsActive = true;
+        femaleIconIsActive = false;
 }
     if(this.id == "femaleIcon") {
         femaleIcon.style = "filter: invert(100%);";
         maleIcon.style = "filter: invert(0%);";
+        maleIconIsActive = false;
+        femaleIconIsActive = true;
 }
 
 }
@@ -85,10 +101,26 @@ function setActive() {
 
 
 function munchkin() {
+    randomID();
+    munchkins.push(myID);
+
+    let isName;
+    name = document.getElementById("munchkinNameInput").value;
+
+    isName = name.trim();
+
+    if(isName == "" || !maleIconIsActive && !femaleIconIsActive) {
+        alert("You need to have both name and gender");
+        return;
+    } else {
+    document.getElementById("munchkinNameInput").value = "";
+    document.getElementById("maleIcon").style = "filter: invert(0%)";
+    document.getElementById("femaleIcon").style = "filter: invert(0%)";
     playerWrapper = document.getElementById("playerWrapper");
     createPlayerCard = document.getElementById("createPlayerCard");
     blur = document.getElementById("blur");
     addButton = document.getElementById("addPlayer");
+
 
     blur.style = "filter: blur(0px);";
     createPlayerCard.style = "display:none;";
@@ -98,20 +130,30 @@ function munchkin() {
     playerCard = document.createElement("div");
     playerCard.className = "player card";
     playerWrapper.appendChild(playerCard);
-
-    randomID();
-    munchkins.push(myID);
-    
-
+   
+   
     playerTitle = document.createElement("div");
+    
     playerTitle.className = "cardTitle";
     playerCard.appendChild(playerTitle);
+    t = document.createTextNode(id);
+    playerTitle.appendChild(t);
+    
 
     nameOfPlayer = document.createElement("h1");
+   
     var t = document.createTextNode(name);
+    var g = document.createElement("IMG");
+    if(maleIconIsActive) {
+    g.src = "./img/maleIcon.png";
+    } else if(femaleIconIsActive) {
+        g.src = "./img/femaleIcon.png";
+    }
+
     nameOfPlayer.appendChild(t);
     playerTitle.appendChild(nameOfPlayer);
-
+    playerTitle.appendChild(g);
+    
     playerBody = document.createElement("div");
     playerBody.className = "playerBody";
     playerCard.appendChild(playerBody);
@@ -120,24 +162,25 @@ function munchkin() {
     playerLevelTitle = document.createElement("h1");
     t = document.createTextNode("Level");
     playerLevelTitle.appendChild(t);
+  
     playerBody.appendChild(playerLevelTitle);
     
-    
-
-
     playerLevelContainer = document.createElement("div");
     playerLevelContainer.className = "playerLevel";
     playerLevelTitle.appendChild(playerLevelContainer);
 
     
-    playerLevel = document.createElement("h1");
-    t = document.createTextNode(level);
-    playerLevel.appendChild(t);
+    playerLevel = document.createElement("h2");
+    plvl = document.createTextNode(level);
+    playerLevel.setAttribute("id", id);
+    
+    playerLevel.appendChild(plvl);
     playerLevelContainer.appendChild(playerLevel);
 
     playerGearTitle = document.createElement("h1");
     t = document.createTextNode("Gear");
     playerGearTitle.appendChild(t);
+    playerGearTitle.className = "playerGearTitle";
     playerBody.appendChild(playerGearTitle);
     
     playerGearContainer = document.createElement("div");
@@ -145,10 +188,66 @@ function munchkin() {
     playerGearTitle.appendChild(playerGearContainer);
 
     
-    playerGear = document.createElement("h1");
+    playerGear = document.createElement("h2");
     t = document.createTextNode(gear);
     playerGear.appendChild(t);
+
+    playerStrengthTitle = document.createElement("h1");
+    t = document.createTextNode("Strength");
+    playerStrengthTitle.appendChild(t);
+    playerStrengthTitle.className = "playerStrengthTitle";
+  
+    playerBody.appendChild(playerStrengthTitle);
+    
+    playerStrengthContainer = document.createElement("div");
+    playerStrengthContainer.className = "playerStrength";
+    playerStrengthTitle.appendChild(playerStrengthContainer);
+
+    
+    playerStrength = document.createElement("h2");
+    t = document.createTextNode(strength);
+    playerStrength.appendChild(t);
+    playerStrengthContainer.appendChild(playerStrength);
+
+
+    var addLevelIcon = document.createElement("button");
+    lp = document.createTextNode("+1");
+    addLevelIcon.className = "addLevelIcon";
+    addLevelIcon.setAttribute("id", id++);
+    addLevelIcon.onclick = (event) => {
+        let playerLvlHtml = document.getElementById(event.target.id);
+        let playerLVL = playerLvlHtml.innerText;
+        playerLVL++;
+        playerLvlHtml.innerHTML = playerLVL;
+        
+    }
+    addLevelIcon.appendChild(lp);
+
+    removeLevelIcon = document.createElement("button");
+    lm = document.createTextNode("-1");
+    removeLevelIcon.className = "removeLevelIcon";
+    removeLevelIcon.setAttribute("id", "removeLevel");
+    removeLevelIcon.appendChild(lm);
+
+    
     playerGearContainer.appendChild(playerGear);
+    playerLevelTitle.appendChild(addLevelIcon);
+    playerLevelTitle.appendChild(removeLevelIcon);
+
+    addGearIcon = document.createElement("button");
+    gp = document.createTextNode("+1");
+    addGearIcon.className = "addGearIcon";
+    addGearIcon.setAttribute("id", "addGear");
+    addGearIcon.appendChild(gp);
+
+    removeGearIcon = document.createElement("button");
+    gm = document.createTextNode("-1");
+    removeGearIcon.className = "removeGearIcon";
+    removeGearIcon.setAttribute("id", "removeGear");
+    removeGearIcon.appendChild(gm);
+
+    playerGearTitle.appendChild(addGearIcon);
+    playerGearTitle.appendChild(removeGearIcon);
     
     
 
@@ -178,11 +277,11 @@ function munchkin() {
         
     }
     
-    
+}
    
 }
 
 function randomID() {
-    myID = Math.random().toString(36).substr(2, 16);
+   myID = Math.random().toString(36).substr(2, 16);
     return myID;
 }
